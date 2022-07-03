978ea0197c5a5dd3109683267fcb6b8af945b914
Arnaud Tron <tron.arnaud@gmail.com>
2022-07-03 14:15:06 +0000

    Updated Notifier

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,13 +2,13 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "CONGRATULATION: Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful ! more info ${env.BUILD_URL}"
+    slackSend color: "good", message: "CONGRATULATION: Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful ! avec un magnifique envol du A320 more info ${env.BUILD_URL}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! more info ${env.BUILD_URL}"
+    slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! DANGER A320 est mort more info ${env.BUILD_URL}"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable ! more info ${env.BUILD_URL}"
+    slackSend color: "warning", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable ! Le cockpit du A320 est en Feu !!! more info ${env.BUILD_URL}"
   }
   else {
     slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear ! more info ${env.BUILD_URL}"	
